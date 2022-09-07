8479c7d47f2cb5231a706cdc21b40403e413e9d6
Chris Horan <horan_chris@hotmail.co.uk>
2022-09-07 09:55:25 +0100

    made a change

--- PythonApplicationSeptArvo/PythonApplicationSeptArvo.py
+++ PythonApplicationSeptArvo/PythonApplicationSeptArvo.py
@@ -1,2 +1,7 @@
-print("hello how are you")
-print()
+print("hello how are you")#this is a print statement
+print("sadfasfas")#this is somethign else
+print("another") #this is not run in code
+print("another") #used for information about the code
+print("another")
+x=3
+print("hello")
